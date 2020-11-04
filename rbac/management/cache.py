1bb220513057a84b403a8091ef900e37af859e95
Keith Walsh <kwalsh@redhat.com>
2020-11-04 15:28:13 -0500

    WIP

--- rbac/management/cache.py
+++ rbac/management/cache.py
@@ -3,6 +3,7 @@
 import contextlib
 import json
 import logging
+import pickle
 
 from django.conf import settings
 from redis import BlockingConnectionPool, exceptions
@@ -12,6 +13,72 @@ logger = logging.getLogger(__name__)  # pylint: disable=invalid-name
 _connection_pool = BlockingConnectionPool(
     max_connections=10, **settings.REDIS_CACHE_CONNECTION_PARAMS  # should match gunicorn.threads
 )
+
+
+class TenantCache:
+    def __init__(self):
+        self._connection = None
+
+    @property
+    def connection(self):
+        """Get Redis connection from the pool."""
+        if not self._connection:
+            self._connection = Redis(connection_pool=_connection_pool)
+            try:
+                self._connection.ping()
+            except exceptions.RedisError:
+                self._connection = None
+                raise
+        return self._connection
+
+    def key_for(self, schema_name):
+        """Redis key for a given tenant."""
+        return f"rbac::tenant::schema={schema_name}"
+
+    def get(self, schema_name):
+        """Get the tenant by schema_name."""
+        try:
+            tenant = self.connection.get(self.key_for(schema_name))
+            if tenant:
+                return pickle.loads(tenant)
+        except exceptions.RedisError:
+            logger.exception(f"Error querying tenant {schema_name}")
+        return None
+
+    def save(self, tenant):
+        """Write the tenant for a request to Redis."""
+        key = self.key_for(tenant.schema_name)
+        try:
+            logger.info(f"Caching tenant {key}")
+            with self.connection.pipeline() as pipe:
+                pipe.set(key, pickle.dumps(tenant))
+                pipe.expire(key, settings.ACCESS_CACHE_LIFETIME)
+                pipe.execute()
+        except exceptions.RedisError:
+            logger.exception(f"Error writing tenant cache for {key}")
+        finally:
+            try:
+                pipe.reset()
+            except:  # noqa: E722
+                pass
+
+    def delete(self, tenant):
+        """Purge the given tenant from the cache."""
+        key = self.key_for(tenant.schema_name)
+        err_msg = f"Error deleting tenant cache {key}"
+        with self.delete_handler(err_msg):
+            logger.info(f"Deleting tenant cache {key}")
+            self.connection.delete(key)
+
+    @contextlib.contextmanager
+    def delete_handler(self, err_msg):
+        """Handle policy delete events."""
+        if not settings.ACCESS_CACHE_ENABLED:
+            return
+        try:
+            yield
+        except exceptions.RedisError:
+            logger.exception(err_msg)
 
 
 class AccessCache:
