ef568a110271069a71cf39e39990a03ce2719f3d
Keith Walsh <kwalsh@redhat.com>
2025-02-13 15:36:22 -0500

    Create a custom queryset on the `Workspace` model for built-ins
    This adds a custom queryset (though maybe we could also consider a manager?) for
    the `Workspace` model, which will help DRY up the queries for built-in types:
    (root + default) for a given tenant.
    
    We override/copy the default `objects` manager, and use `.as_manager` [1] to include
    the custom queryset method.
    
    ```python
    built_in_workspaces = Workspace.objects.built_in(tenant=tenant)
    ```
    
    This can be chained as well, like `built_in.filter(...)` if needed, as it returns
    a queryset object.
    
    [1] https://docs.djangoproject.com/en/5.1/topics/db/managers/#creating-a-manager-with-queryset-methods

--- rbac/management/workspace/model.py
+++ rbac/management/workspace/model.py
@@ -25,6 +25,14 @@ from management.rbac_fields import AutoDateTimeField
 from api.models import TenantAwareModel
 
 
+class BuiltInWorkspaceQuerySet(models.QuerySet):
+    """A custom queryset for built-in workspaces."""
+
+    def built_ins(self, tenant):
+        """Return a queryset of built-in workspaces for a tenant."""
+        return self.filter(tenant=tenant, type__in=[Workspace.Types.ROOT, Workspace.Types.DEFAULT])
+
+
 class Workspace(TenantAwareModel):
     """A workspace."""
 
@@ -40,6 +48,8 @@ class Workspace(TenantAwareModel):
     type = models.CharField(choices=Types.choices, default=Types.STANDARD, null=False)
     created = models.DateTimeField(default=timezone.now)
     modified = AutoDateTimeField(default=timezone.now)
+
+    objects = BuiltInWorkspaceQuerySet.as_manager()
 
     class Meta:
         ordering = ["name", "modified"]
